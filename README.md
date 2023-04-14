730359748e359eafb41b0c9a242f750d126037fe
AmberDuong <u3235727@uni.canberra.edu.au>
2023-04-14 23:10:44 +1000

    Finished Website

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# 11056-website
- assignment 1
+# 11056 - CANBERRA MODERN WEBBSITE
+![HomePage](assets/images/HomePage.png)
+
+## REFLECTION:
+The 'Canberra Modern' Website is built to showcase Canberra’s unique mid – late twentieth century places and spaces. 
+
+The goal of the website is to draw inspiration from, and reflect the personality of, Canberra as a modernist city. To achieve this, I took full advantage of CM colours for the website content and pages and experimented with different div shapes.
+
+Having learned about HTML and CSS before this unit, creating the structure for the website was not the challenging part. Designing the layout that fulfill the needs of CM made me struggle.
+
+Overall, the finished website is similar to the low-fi layout prototype with small changes to fit the size of the content. 
+
+Although I have successfully built the website I envisioned in the concept developing stage, I find that the website still has room to grow in terms of layout and visuals to reflect the fun and playful style of the program. 
+
+If I could redo this project, I would invest more in the concept ideation, experiment with different compositions and think outside the box.
+
+![PrototypeImage](assets/images/Low-fi prototype.jpg)
--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,8 @@
+// read more/read less button
 let noOfCharac = 0;
 let contents = document.querySelectorAll(".content");
 contents.forEach(content => {
+    // if the text length is less than noOfCharac, hide the read more button
     if(content.textContent.length < noOfCharac){
         content.nextElementSibling.style.display = "none";
     }
@@ -18,35 +20,37 @@ function readMore(btn){
     btn.textContent == "Read More" ? btn.textContent = "Read Less" : btn.textContent = "Read More";
 }
 
-const slider = document.querySelector('.slider');
-
-const leftArrow = document.querySelector('.left');
-const rightArrow = document.querySelector('.right');
+// carousel slider
+const slider = document.querySelector(".slider");
+const leftArrow = document.querySelector(".left");
+const rightArrow = document.querySelector(".right");
 const indicatorParents = document.querySelector('.controls ul');
 var sectionIndex = 0;
 
 function setIndex() {
-    document.querySelector('.controls .selected').classList.remove('selected');
-    slider.style.transform = 'translate(' + (sectionIndex) * -25 + '%)';
+    document.querySelector(".controls .selected").classList.remove("selected");
+    slider.style.transform = "translate(" + (sectionIndex) * -25 + "%)";
 }
 
-document.querySelectorAll('.controls li').forEach(function(indicator, ind) {
-    indicator.addEventListener('click', function() {
+document.querySelectorAll(".controls li").forEach(function(indicator, ind) {
+    indicator.addEventListener("click", function() {
         sectionIndex = ind;
         setIndex();
-        indicator.classList.add('selected');
+        indicator.classList.add("selected");
     });
 });
 
-leftArrow.addEventListener('click', function() {
+// if left arrow is clicked and there is space to more, translate the section to the left by 25%
+leftArrow.addEventListener("click", function() {
     sectionIndex = (sectionIndex > 0) ? sectionIndex - 1 : 0;
-    indicatorParents.children[sectionIndex].classList.add('selected');
+    indicatorParents.children[sectionIndex].classList.add("selected");
     setIndex();
 });
 
-rightArrow.addEventListener('click', function() {
+// if right arrow is clicked and there is space to more, translate the section to the right by 25%
+rightArrow.addEventListener("click", function() {
     sectionIndex = (sectionIndex < 3) ? sectionIndex + 1 : 3;
-    indicatorParents.children[sectionIndex].classList.add('selected');
+    indicatorParents.children[sectionIndex].classList.add("selected");
     setIndex();
 });
 
